5fd8a0581f63247432bf8ecaf093d7cc064c57a3
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2024-03-02 17:27:45 +0100

    caricamento esercizio

---
+++ unittest/src/main/java/com/unittest/unittest/services/UserService.java
@@ -0,0 +1,49 @@
+package com.unittest.unittest.services;
+
+import com.unittest.unittest.entities.User;
+import com.unittest.unittest.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        User savedUser = userRepository.save(user);
+        return savedUser;
+    }
+
+    public List<User> getAllUser() {
+        List<User> users = (List<User>) userRepository.findAll();
+        return users;
+    }
+
+    public Optional<User> getUser(Long id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        return userOptional;
+    }
+
+    public Optional<User> updateUser(Long id,User user){
+        Optional<User> userDaAggiornare = userRepository.findById(id);
+        if (userDaAggiornare.isPresent()){
+            userDaAggiornare.get().setName(user.getName());
+            userDaAggiornare.get().setSurname(user.getSurname());
+            userRepository.save(userDaAggiornare.get());
+        } else {
+            return Optional.empty();
+        }
+        return userDaAggiornare;
+    }
+
+
+    public User deleteUser(Long id, User user){
+        userRepository.deleteById(id);
+        return user;
+    }
+}
